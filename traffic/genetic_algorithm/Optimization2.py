47ac1466ae63fea97cdf6af5f6695b5da784e797
Sajeerat <sajeerat@Sajeerats-MacBook-Pro.local>
2019-04-23 21:05:13 +0700

    Fix file path

--- traffic/genetic_algorithm/Optimization2.py
+++ traffic/genetic_algorithm/Optimization2.py
@@ -1,6 +1,6 @@
-from long import GA1
-from short1 import GA2
-from simulator import Simulator
+from .long import GA1
+from .short1 import GA2
+from .simulator import Simulator
 import time
 from deap import tools
 import math
@@ -9,37 +9,41 @@ import os.path
 import pickle
 import numpy as np
 
+
 class Controller:
     def __init__(self, params):
         self.params = params
         self.params["simulator"].setTimeInterval(self.params["intervalSize"])
         self.timeSteps = params["timeSteps"]
-        self.paramsListGA2 = ["crossover", "mutate", "select", "populationGA2", "numGeneration2", "crossroads", "numIndividuals2", "fitnessGA2", "simulator", "densities", "minLim", "maxLim"]
-        self.paramsGA2 = dict((k, params[k]) for k in self.paramsListGA2 if k in params)
+        self.paramsListGA2 = ["crossover", "mutate", "select", "populationGA2", "numGeneration2",
+                              "crossroads", "numIndividuals2", "fitnessGA2", "simulator", "densities", "minLim", "maxLim"]
+        self.paramsGA2 = dict((k, params[k])
+                              for k in self.paramsListGA2 if k in params)
         self.positions = {}
         self.params["simulator"].clear()
-        #change simulation state
+        # change simulation state
 
     def run1(self):
         newPopulation = self.params["populationGA2"]
         if newPopulation is not None:
             bestIndividual = newPopulation[0][0:self.params["crossroads"]]
-        
-        
+
         fitness = 0
         for timeStep in range(self.params["timeSteps"]):
             if newPopulation is not None:
                 population = []
                 for individual in newPopulation:
-                    population.append(individual[timeStep*self.params["crossroads"]:(timeStep+1)*self.params["crossroads"]])
+                    population.append(
+                        individual[timeStep*self.params["crossroads"]:(timeStep+1)*self.params["crossroads"]])
             else:
                 population = None
             print("Timtestep: " + str(timeStep))
             self.paramsGA2["population"] = population
             ga2 = GA2(self.paramsGA2)
             best, bestIndividual = ga2.run()
-            fitness+=best
-            self.positions[timeStep] = self.params["simulator"].getPositions(bestIndividual[0], True)
+            fitness += best
+            self.positions[timeStep] = self.params["simulator"].getPositions(
+                bestIndividual[0], True)
             self.params["simulator"].setState(bestIndividual)
             print(self.positions)
         print(fitness)
@@ -51,7 +55,8 @@ class Controller:
         if newPopulation is not None:
             population = []
             for individual in newPopulation:
-                population.append(individual[timeStep*self.params["crossroads"]:(timeStep+1)*self.params["crossroads"]])
+                population.append(
+                    individual[timeStep*self.params["crossroads"]:(timeStep+1)*self.params["crossroads"]])
         else:
             population = None
         self.paramsGA2["population"] = population
@@ -62,15 +67,13 @@ class Controller:
         return improvement, bestIndividual[0], positions
 
 
-
-##should look something like this on front end
-
-##Sample params
-##params = {"numGeneration1": 10,
-##          "timeSteps": 10,
-##          "intervalSize": 120,
-##          "numIndividuals2": 50,
-##          "populationGA2": obtained from optimzation1}
+# should look something like this on front end
+# Sample params
+# params = {"numGeneration1": 10,
+# "timeSteps": 10,
+# "intervalSize": 120,
+# "numIndividuals2": 50,
+# "populationGA2": obtained from optimzation1}
 params = {"numGeneration2": 10,
           "timeSteps": 10,
           "intervalSize": 120,
@@ -78,7 +81,7 @@ params = {"numGeneration2": 10,
           "populationGA2": [[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]]}
 NUM_INDIVIDUALS = params["numIndividuals2"]
 preDefinedParams = {"crossover": {"operator": tools.cxTwoPoint},
-                    "mutate": {"operator": tools.mutShuffleIndexes,"indpb": 0.1},
+                    "mutate": {"operator": tools.mutShuffleIndexes, "indpb": 0.1},
                     "select": {"operator": tools.selRoulette, "k": int(math.sqrt(NUM_INDIVIDUALS//2))},
                     "crossroads": 21,
                     "densities": None,
@@ -86,7 +89,7 @@ preDefinedParams = {"crossover": {"operator": tools.cxTwoPoint},
                     "fitnessGA2": "1",
                     "minLim": 0,
                     "maxLim": 119}
-                        
+
 controller = Controller({**params, **preDefinedParams})
 for i in range(params["timeSteps"]):
     controller.run(i)
--- traffic/genetic_algorithm/short1.py
+++ traffic/genetic_algorithm/short1.py
@@ -1,6 +1,6 @@
 import array
 import random
-from long import GA1
+from .long import GA1
 import numpy
 import math
 from deap import algorithms
@@ -8,6 +8,7 @@ from deap import base
 from deap import creator
 from deap import tools
 import copy
+
 
 class GA2:
     def __init__(self, params):
@@ -25,7 +26,8 @@ class GA2:
         self.fitness = params["fitnessGA2"]
         self.simulator.useSave = True
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-        creator.create("Individual", array.array, typecode='b', fitness=creator.FitnessMin)
+        creator.create("Individual", array.array, typecode='b',
+                       fitness=creator.FitnessMin)
 
         self.toolbox = base.Toolbox()
 
@@ -33,8 +35,10 @@ class GA2:
         self.toolbox.register("random", random.randint, minLim, maxLim)
 
         # Structure initializers
-        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.random, self.crossroads)
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+        self.toolbox.register("individual", tools.initRepeat,
+                              creator.Individual, self.toolbox.random, self.crossroads)
+        self.toolbox.register("population", tools.initRepeat,
+                              list, self.toolbox.individual)
         self.toolbox.register("evaluate", self.fitnessFunction)
         self.toolbox.register("mate", self.crossover["operator"])
         del self.crossover["operator"]
@@ -45,16 +49,18 @@ class GA2:
 
     def fitnessFunction(self, population):
         fitnesses = [(3, )]*self.numIndividuals
-        signals = numpy.ndarray((self.numIndividuals, 1, self.crossroads), dtype = numpy.uint8)
+        signals = numpy.ndarray(
+            (self.numIndividuals, 1, self.crossroads), dtype=numpy.uint8)
         for individual in range(self.numIndividuals):
             for timeStep in range(1):
-                timings = population[individual][self.crossroads*timeStep:self.crossroads*(timeStep+1)]
+                timings = population[individual][self.crossroads *
+                                                 timeStep:self.crossroads*(timeStep+1)]
                 signals[individual][timeStep] = timings
-        if self.fitness=="1":
+        if self.fitness == "1":
             fitnesses = self.simulator.getFitness1(signals)
-        elif self.fitness=="2":
+        elif self.fitness == "2":
             fitnesses = self.simulator.getFitness2(signals)
-        elif self.fitness=="3":
+        elif self.fitness == "3":
             fitnesses = self.simulator.getFitness3(signals, self.densities)
         return fitnesses
 
@@ -68,14 +74,14 @@ class GA2:
 ##        print("Generation 1")
         fitnesses = self.fitnessFunction(pop)
         for ind, fit in zip(pop, fitnesses):
-##            print(ind, fit)
+            ##            print(ind, fit)
             ind.fitness.values = fit
 
         worst = min([ind.fitness.values[0] for ind in pop])
         bestFitness = 0
         worstFitness = 0
         bestIndividual = None
-        
+
         for generation in range(self.numGeneration):
             length = len(pop)
             fits = [ind.fitness.values[0] for ind in pop]
@@ -85,7 +91,7 @@ class GA2:
             improvement = ((worst-min(fits))*100)/worst
             bestFitness = min(fits)
             worstFitness = max(fits)
-            
+
             print("-"*30)
             print("Generation %s statistics" % str(generation+1))
             print("          Min: %s" % min(fits))
@@ -98,10 +104,10 @@ class GA2:
             params_select["individuals"] = pop
             bestIndividuals = self.toolbox.select(**params_select)
 ##            print("Selected individuals:")
-##            print(bestIndividuals)
+# print(bestIndividuals)
             bestIndividual = bestIndividuals[0]
             del params_select
-            if (generation==self.numGeneration-1):
+            if (generation == self.numGeneration-1):
                 break
             offspring = pop.copy()
             index = 0
@@ -110,9 +116,9 @@ class GA2:
                 for child2 in bestIndividuals:
                     temp1 = child1.__deepcopy__({})
                     temp2 = child2.__deepcopy__({})
-                    if(temp1==temp2):
+                    if(temp1 == temp2):
                         offspring[index] = temp1
-                        index+=1
+                        index += 1
                     else:
                         params_crossover = copy.deepcopy(self.crossover)
                         params_crossover["ind1"] = temp1
@@ -121,23 +127,23 @@ class GA2:
                         del params_crossover
                         offspring[index] = temp1
                         offspring[index+1] = temp2
-                        index+=2
-                    if (index>=len(offspring)-2):
+                        index += 2
+                    if (index >= len(offspring)-2):
                         break
-                if (index>=len(offspring)-2):
+                if (index >= len(offspring)-2):
                     break
-##            for mutant in offspring:
+# for mutant in offspring:
 ##                params_mutate = copy.deepcopy(self.mutate)
 ##                params_mutate["individual"] = mutant
-##                self.toolbox.mutate(**params_mutate)
+# self.toolbox.mutate(**params_mutate)
 ##                del params_mutate
 
 ##            print("Generation " + str(generation+2))
             fitnesses = self.fitnessFunction(offspring)
             for ind, fit in zip(offspring, fitnesses):
-##                print(ind, fit)
+                ##                print(ind, fit)
                 ind.fitness.values = fit
-                
+
             pop[:] = offspring
             fits = [ind.fitness.values[0] for ind in pop]
         return bestFitness, (worstFitness - bestFitness)*100/worstFitness, bestIndividual
